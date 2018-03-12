475831728dea5baf975e26992330c3f260125ea0
evgfilim1 <evgfilim1@yandex.ru>
2018-03-12 16:29:24 +0500

    Now the code can be launched

---
+++ main.py
@@ -0,0 +1,9 @@
+from sys import argv, exit
+from PyQt5.QtWidgets import QApplication
+from main_ui import MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(argv)
+    window = MainWindow()
+    window.show()
+    exit(app.exec())
