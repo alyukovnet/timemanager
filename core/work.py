b8b12e7f78c218d8a30433c3d1167fd02cfcb30c
evgfilim1 <evgfilim1@yandex.ru>
2018-03-13 17:22:17 +0000

    Updated core.work

--- core/work.py
+++ core/work.py
@@ -11,9 +11,10 @@ gum_net = NeuralNetwork()
 
 def load():
     global phys_lessons, chem_lessons, gum_lessons, work_queue
-    with open('lessons', 'rb') as f1, open('queue', 'rb') as f2:
-        phys_lessons, chem_lessons, gum_lessons = pickle.load(f1)
-        work_queue = pickle.load(f2)
+    with open('lessons', 'rb') as f:
+        phys_lessons, chem_lessons, gum_lessons = pickle.load(f)
+    with open('queue', 'rb') as f:
+        work_queue = pickle.load(f)
 
 
 def what_network(x):
@@ -21,6 +22,22 @@ def what_network(x):
     for borders, net in neural_types.items():
         if borders[0] <= x <= borders[1]:
             return nets[net]
+
+
+def clear_queue():
+    global work_queue
+    work_queue = []
+    with open('queue', 'wb') as f:
+        pickle.dump(work_queue, f)
+
+
+def clear_nn():
+    global phys_lessons, chem_lessons, gum_lessons
+    phys_lessons = []
+    chem_lessons = []
+    gum_lessons = []
+    with open('queue', 'wb') as f:
+        pickle.dump(work_queue, f)
 
 
 def train():
@@ -43,10 +60,6 @@ def deadline(date_deadline):
     return (date_deadline - date.today()).days
 
 
-def show_queue():
-    return 0
-
-
 def to_queue(lesson, work_type, date_start, date_end, time_m, pri):
     inputs = [actual(date_start), deadline(date_end), time_m, pri]
     predict = what_network(lesson).predict(np.array(inputs))[0]
@@ -54,26 +67,26 @@ def to_queue(lesson, work_type, date_start, date_end, time_m, pri):
     work_queue.append(line)
     with open('queue', 'wb') as f:
         pickle.dump(work_queue, f)
-    show_queue()
 
 
 def refresh_queue():
+    train()
     for line in work_queue:
         inputs = [actual(line[2]), deadline(line[3]), line[4], line[5]]
         line[-1] = what_network(line[0]).predict(np.array(inputs))[0]
     with open('queue', 'wb') as f:
         pickle.dump(work_queue, f)
-    show_queue()
 
 
-def new_train():
+def new_train(queue_id):
+    line = work_queue[queue_id]
+    what_network(line[0]).append([[actual(line[2]), deadline(line[3]), line[4], line[5]], line[6]])
     with open('data.pickle', 'wb') as f:
         pickle.dump([phys_lessons, chem_lessons, gum_lessons], f)
 
 
-def queue_del(x):
-    work_queue.pop(x)
-    show_queue()
+def queue_del(queue_id):
+    work_queue.pop(queue_id)
 
 
 def main():
