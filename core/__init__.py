05b38071e2c447d6862a755e442eef34fd027cda
evgfilim1 <evgfilim1@yandex.ru>
2018-03-15 20:52:29 +0500

    Core refactoring: now with classes

---
+++ core/__init__.py
@@ -0,0 +1,91 @@
+from .neural import NeuralNetwork
+from .data import neural_types
+from datetime import date
+import numpy as np
+import pickle
+
+
+class LessonQueue:
+    def __init__(self):
+        try:
+            with open('queue', 'rb') as f:
+                self._data = pickle.load(f)
+        except FileNotFoundError:
+            self._data = []
+        self.neural = NNStorage()
+
+    def __delitem__(self, key):
+        self._data.pop(key)
+        self._dump()
+
+    def _dump(self):
+        with open('queue', 'wb') as file:
+            pickle.dump(self._data, file)
+
+    @property
+    def data(self):
+        return self._data
+
+    def add(self, lesson, work_type, start, end, time, priority):
+        inputs = [actual(start), deadline(end), time, priority]
+        predict = self.neural.choose_type(lesson).predict(np.array(inputs))[0]
+        line = [lesson, work_type, start, end, time, priority, predict]  # Memory line
+        self._data.append(line)
+        self._dump()
+
+    def clear(self):
+        self._data.clear()
+        self._dump()
+
+    def refresh(self):
+        self.neural.train()
+        for line in self._data:
+            inputs = [actual(line[2]), deadline(line[3]), line[4], line[5]]
+            line[-1] = self.neural.choose_type(line[0]).predict(np.array(inputs))[0]
+        self._dump()
+
+
+class NNStorage:
+    def __init__(self):
+        # lessons, networks: phys, chem, gum
+        try:
+            with open('lessons', 'rb') as f:
+                self._lessons = pickle.load(f)
+        except FileNotFoundError:
+            self._lessons = [[], [], []]
+        self._networks = NeuralNetwork(), NeuralNetwork(), NeuralNetwork()
+
+    def choose_type(self, x):
+        for borders, net in neural_types.items():
+            if borders[0] <= x <= borders[1]:
+                return self._networks[net]
+
+    def clear(self):
+        for i in range(len(self._lessons)):
+            self._lessons[i].clear()
+        self.train()
+
+    def train(self):
+        for net, lesson in zip(self._networks, self._lessons):
+            for _ in range(5000):
+                for input_stat, correct_predict in lesson:
+                    net.train(np.array(input_stat), correct_predict)
+
+    def _dump(self):
+        with open('lessons', 'wb') as file:
+            pickle.dump(self._lessons, file)
+
+    def add(self, queue_line):
+        self.choose_type(queue_line[0]).append([
+            [actual(queue_line[2]), deadline(queue_line[3]), queue_line[4], queue_line[5]],
+            queue_line[6]
+        ])
+        self._dump()
+
+
+def actual(date_start):
+    return (date.today() - date_start).days
+
+
+def deadline(date_deadline):
+    return (date_deadline - date.today()).days
--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from sys import argv, exit
 from PyQt5.QtWidgets import QApplication
 from ui import MainWindow
 
+
 if __name__ == '__main__':
     app = QApplication(argv)
     window = MainWindow()
+    data = window.centralWidget().head_widget
     window.show()
-    exit(app.exec())
+    window.statusBar().showMessage('Готов')
+    status = app.exec()
+    exit(status)
